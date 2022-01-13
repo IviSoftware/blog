06948d1fc35d0d3a99242e2074f22ea9a1164778
ivi <iva_men_ase@hotmail.com>
2022-01-13 17:18:25 -0600

    Todo listo para despliegue

--- config/conexion.js
+++ config/conexion.js
@@ -1,11 +1,14 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'blog',
-    password: 'ivi1234',
+    user: 'gdviosxxgglwxc',
+    host: 'ec2-34-205-209-14.compute-1.amazonaws.com',
+    database: 'd9mck2v8c6vpv0',
+    password: '0624d715669b98338dcd96f40e35f1bb9bf314595ab00a73ba73983fe8f4e080',
     port: 5432,
+    ssl: {
+      rejectUnauthorized: false,
+    }
   });
 
 module.exports = pool;
